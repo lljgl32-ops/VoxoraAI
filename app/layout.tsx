234c04c657c6d3e9a168c62fa7b864b69edb0eb6
lljgl32-ops <lljgl32@gmail.com>
2026-07-14 02:40:22 +0330

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Voxora AI",
+  description: "Create AI Videos in Minutes",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+    }
