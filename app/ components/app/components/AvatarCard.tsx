b1b83a5c5053b5388197a5aa33d05ff674115e04
lljgl32-ops <lljgl32@gmail.com>
2026-07-14 22:40:54 +0330

    Create AvatarCard.tsx

---
+++ app/ components/app/components/AvatarCard.tsx
@@ -0,0 +1,19 @@
+export default function AvatarCard() {
+  return (
+    <div style={{
+      background: "#fff",
+      padding: "15px",
+      borderRadius: "12px",
+      textAlign: "center"
+    }}>
+      <img 
+        src="/avatar1.jpg" 
+        alt="avatar" 
+        style={{ width: "100%", borderRadius: "10px" }}
+      />
+
+      <h3>AI Avatar</h3>
+      <p>Realistic Speaker</p>
+    </div>
+  );
+}
