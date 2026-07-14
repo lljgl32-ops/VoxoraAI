45c7c278197df272142b6983662b77042f47d1ac
lljgl32-ops <lljgl32@gmail.com>
2026-07-14 22:43:56 +0330

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import AvatarCard from "../components/AvatarCard";
 export default function Home() {
   return (
     <main className="min-h-screen bg-gradient-to-b from-blue-100 to-white text-gray-900">
